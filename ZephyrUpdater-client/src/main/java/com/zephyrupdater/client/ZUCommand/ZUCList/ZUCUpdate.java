9d0e831d374a7ba35c557377046287169f75ba89
LEININGER Mathys <156470119+Thysma28@users.noreply.github.com>
2024-01-15 22:00:25 +0100

    [client] ZUCUpdate isDirectory part 1

--- ZephyrUpdater-client/src/main/java/com/zephyrupdater/client/ZUCommand/ZUCList/ZUCUpdate.java
+++ ZephyrUpdater-client/src/main/java/com/zephyrupdater/client/ZUCommand/ZUCList/ZUCUpdate.java
@@ -1,13 +1,22 @@
 package com.zephyrupdater.client.ZUCommand.ZUCList;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.zephyrupdater.client.AppClient;
+import com.zephyrupdater.client.MainClient;
 import com.zephyrupdater.client.ZUCommand.ZUCStruct;
+import com.zephyrupdater.common.CommonUtil;
 import com.zephyrupdater.common.ZUCommandCore.ZUCList.ZUCUpdateCore;
 import com.zephyrupdater.common.ZUProtocolCore.ZUPManager;
 import com.zephyrupdater.common.ZUProtocolCore.ZUProtocolTypesCore.ZUPCommandCore;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
 import java.util.List;
+import java.util.Objects;
 
 public class ZUCUpdate extends ZUCUpdateCore implements ZUCStruct {
     public ZUCUpdate(JsonObject data) {
@@ -20,16 +29,55 @@ public class ZUCUpdate extends ZUCUpdateCore implements ZUCStruct {
 
     @Override
     public void executeServerCmd() {
-        /* TODO
-        *
-        * check files (if they need to be update)
-        *
-        * */
+        String clientMainPath = String.valueOf(MainClient.clientFilePath);
 
-        System.out.println("Update file at: " + this.folderPath);
-        System.out.println(this.filesJson.toString());
+        for (String key : this.filesJson.keySet()) {
+            JsonElement jsonElement = this.filesJson.get(key);
+
+            if (!filesJson.isJsonObject()) {
+                System.out.println(this.filesJson + " is not a JsonObject");
+            }
+
+            JsonObject midJson = jsonElement.getAsJsonObject();
+            System.out.println("Update " + key);
+
+            Path path = Paths.get(CommonUtil.getValueFromJson("path", midJson, String.class));
+            String hash = CommonUtil.getValueFromJson("hash", midJson, String.class);
+
+            System.out.println("Server path is : " + path);
+            System.out.println("File hash is :" + hash);
+
+            Path localPath = Paths.get(MainClient.clientFilePath.toString() + path);
+            if(Files.exists(localPath) && Files.isDirectory(localPath)){
+                System.out.println(key + "is downloaded");
+                System.out.println("hash verification :");
+                try {
+                    byte[] fileContentBytes = Files.readAllBytes(path);
+                    byte[] clienthash = MessageDigest.getInstance("SHA-256").digest(fileContentBytes);
+
+                    if(!Objects.equals(clienthash, hash)){
+                        System.out.println("not same hash, delete it");
+                        /* TODO
+                        delete file and doawnload a new one
+                         */
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            else{
+                System.out.println("No such file or directory");
+                /* TODO
+                doawnload it
+                 */
+            }
+
+
+
+
+
+        }
     }
-
     /**
      * Checking files at a specific folder
      * path format: Arffornia_V.5/clientFiles/
